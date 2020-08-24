69e516df9cb5fbf220213a50cf8b4af458dad3d7
Mike Hornung <mike@qed.fyi>
2020-08-24 12:53:34 -0700

    Update stream channel name

--- livestream/livestream.sh
+++ livestream/livestream.sh
@@ -3,5 +3,5 @@
 ffmpeg -nostdin -hide_banner -nostats -loglevel quiet  \
   -f v4l2 -format mjpeg -framerate 30 -video_size 1280x720  \
   -i /dev/video0 -codec:v h264_omx -b:v 2M -bufsize 1M -filter:v fps=fps=30  \
-  -f flv rtmp://127.0.0.1/birdcam/live
+  -f flv rtmp://127.0.0.1/cam/live
 
